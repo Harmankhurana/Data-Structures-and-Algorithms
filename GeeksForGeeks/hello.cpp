36fef886485fbf34da67b4c7be06d6c228f7d7d4
Harman khurana <work.harmankhurana@gmail.com>
2025-08-03 04:26:39 +0530

    Basic C++ coding pratice

---
+++ GeeksForGeeks/hello.cpp
@@ -0,0 +1,8 @@
+// Writing my first C++ code 
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World";
+    return 0;
+}
---
+++ GeeksForGeeks/inp-out.cpp
@@ -0,0 +1,12 @@
+// Return product of two integers - A and B
+#include<iostream>
+using namespace std;
+
+int multiplication(int A, int B){
+    return A * B;
+}
+
+int main(){
+    cout << multiplication(2,3);
+    return 0;
+}
