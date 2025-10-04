99ab47ea5130583eab8a19cf8b938cd1bc10c79f
Harman khurana <work.harmankhurana@gmail.com>
2025-10-04 16:15:02 +0530

    Bubble Sort

--- C++ Programming/bubble-sort.cpp
+++ C++ Programming/bubble-sort.cpp
@@ -4,12 +4,16 @@ using namespace std;
 // function for bubble sort
 void bubble_sort(int arr[] , int n){
     for(int i = n-1 ; i >=0 ; i--){
+        int didSwap = 0;
         for(int j = 0 ; j <= i-1 ; j++){
             if(arr[j] > arr[j+1]){
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
             }
+        }
+        if(didSwap == 0){
+            break;
         }
     }
 }
