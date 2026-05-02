4dbd3589c7e1b9499cc7db16ca70a2233b496079
Harman khurana <work.harmankhurana@gmail.com>
2026-05-02 18:17:35 +0530

    Writing a program for printing 1 to N numbers by using loops

---
+++ Campus Recruitment Training/CRT-Phase-1/loop-1-to-N.cpp
@@ -0,0 +1,20 @@
+// A program for printing 1 to N numbers by using Loops
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    int n;
+    cout << "Enter Your Number Here: ";
+    cin >> n;
+
+    // Making a while loop
+    while(i <= n){
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
+
+// Input - Enter Your Number Here: 5
+// output - 1 2 3 4 5
