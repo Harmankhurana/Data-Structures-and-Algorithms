0a95182080d8f66fe9028c2d50eef178bdf2b748
Harman khurana <work.harmankhurana@gmail.com>
2026-05-03 21:23:55 +0530

    A program for normal input/output in C++ programming

---
+++ Striver A2Z DSA Sheet/Basic/input-output.cpp
@@ -0,0 +1,12 @@
+// Writing Input/Output
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cout << "Enter the Value of X: ";
+    cout << "Enter the value of Y: ";
+    cin >> x >> y;
+    cout << "Value of X is: " << x << "Value of Y is: " << y;
+    return 0;
+}
