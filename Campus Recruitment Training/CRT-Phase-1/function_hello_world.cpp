c429feb9b5212f54c869f8babe9153e3ff8eec15
Harman khurana <work.harmankhurana@gmail.com>
2026-05-05 20:39:15 +0530

    A function that prints Hello World!

---
+++ Campus Recruitment Training/CRT-Phase-1/function_hello_world.cpp
@@ -0,0 +1,14 @@
+// A function that prints "Hello, World!"
+#include<iostream>
+using namespace std;
+
+// Creating a function
+void HelloWorld() {
+    cout << "Hello World" << endl;
+}
+
+int main() {
+    // calling function
+    HelloWorld();
+    return 0;
+}
