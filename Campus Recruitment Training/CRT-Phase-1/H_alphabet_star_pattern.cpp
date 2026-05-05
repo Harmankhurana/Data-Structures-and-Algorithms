1b98778b169c73e7a748d441dbba2b94add26b62
Harman khurana <work.harmankhurana@gmail.com>
2026-05-05 09:44:05 +0530

    A Program for printing H alphabet star pattern

---
+++ Campus Recruitment Training/CRT-Phase-1/H_alphabet_star_pattern.cpp
@@ -0,0 +1,32 @@
+// A Program for printing H alphabet star pattern
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter Your Number Here: ";
+    cin >> n;
+    int mid = (n+1)/2; // formula for taking out mid
+
+    for (int i = 1 ; i <= n ; i++) {
+        for (int j = 1 ; j <= n ; j++) {
+            // If-else statement
+            if(j == 1 || j == n || i == mid){
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+// input - Enter Your Number Here: 5
+// output -
+/*
+    *   *
+    *   *
+    *****
+    *   *
+    *   *
+*/
