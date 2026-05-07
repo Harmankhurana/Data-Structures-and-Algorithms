e96059e3afa98a6332eec32b34be4b3d3024036f
Harman khurana <work.harmankhurana@gmail.com>
2026-05-07 19:22:57 +0530

    A program that takes an input of age and prints if you're an adult or not

---
+++ Striver A2Z DSA Sheet/Basic/adult-or-not.cpp
@@ -0,0 +1,16 @@
+// A program that takes an input of age and prints if you're an adult or not
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter Your Age Here: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "Yes, you're an adult";
+    } else {
+        cout << "No you're not an adult";
+    }
+    return 0;
+}
