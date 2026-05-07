3b6beba0b9ee8b89c1be76414533a1fcd2019873
Harman khurana <work.harmankhurana@gmail.com>
2026-05-07 20:10:02 +0530

    A program for job eligibility

--- Striver A2Z DSA Sheet/Basic/job-eligibility.cpp
+++ Striver A2Z DSA Sheet/Basic/job-eligibility.cpp
@@ -1,8 +1,8 @@
 /*
     take the age from the user and then decide accordingly
-    1. if age < 18, print -> not elgible for job
-    2. if age >= 18, print -> elgible for job
-    3. if age >= 55 && age <= 57, print -> elgible for job, but retirement soon
+    1. if age < 18, print -> not eligible for job
+    2. if age >= 18 && age <= 54, print -> eligible for job
+    3. if age >= 55 && age <= 57, print -> eligible for job, but retirement soon
     4. if age >= 58, print -> retirement time
 
 */
@@ -11,5 +11,22 @@
 using namespace std;
 
 int main() {
-    
+    int age;
+    cout << "Enter your age here: " << endl;
+    cin >> age;
+
+    if (age < 18) {
+        cout << "Not eligible for job" << endl;
+    }
+    else if(age >= 18 && age <= 54) {
+        cout << "eligible for job" << endl;
+    }
+    else if(age >= 55 && age <= 57) {
+        cout << "eligible for job, but retirement soon" << endl;
+    }
+    else{
+        cout << "retirement time" << endl;
+    }
+
+    return 0;
 }
