1c3623d57cf5113c9ac81c4ce1cba33b4af31006
Harman khurana <work.harmankhurana@gmail.com>
2026-05-07 18:43:30 +0530

    Writing input/output code in CPP

--- Striver A2Z DSA Sheet/Basic/input-output.cpp
+++ Striver A2Z DSA Sheet/Basic/input-output.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main() {
     int x, y;
+
     cout << "Enter the Value of X: ";
+    cin >> x;
+
     cout << "Enter the value of Y: ";
-    cin >> x >> y;
+    cin >> y;
+    
     cout << "Value of X is: " << x << "Value of Y is: " << y;
     return 0;
 }
