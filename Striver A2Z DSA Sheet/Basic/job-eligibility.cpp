3b43fded6c051a735b483ef10ff637f0d2d7f636
Harman khurana <work.harmankhurana@gmail.com>
2026-05-07 20:13:55 +0530

    using nested loops for job eligibility

--- Striver A2Z DSA Sheet/Basic/job-eligibility.cpp
+++ Striver A2Z DSA Sheet/Basic/job-eligibility.cpp
@@ -18,11 +18,11 @@ int main() {
     if (age < 18) {
         cout << "Not eligible for job" << endl;
     }
-    else if(age >= 18 && age <= 54) {
-        cout << "eligible for job" << endl;
-    }
-    else if(age >= 55 && age <= 57) {
-        cout << "eligible for job, but retirement soon" << endl;
+    else if (age <=57) {
+        cout << "eligible of job";
+        if (age >=55) {
+            cout << ", but retirement soon";
+        }
     }
     else{
         cout << "retirement time" << endl;
