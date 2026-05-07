70214a4df7c5dd25f58cd0409aa09a4ab52d0e06
Harman khurana <work.harmankhurana@gmail.com>
2026-05-07 21:20:52 +0530

    Understading basic arrays in CPP language

--- Striver A2Z DSA Sheet/Basic/basic-array.cpp
+++ Striver A2Z DSA Sheet/Basic/basic-array.cpp
@@ -1,1 +1,23 @@
 // Understanding basic arrays
+#include<iostream>
+using namespace std;
+
+int main() {
+    // taking the size of the array(5)
+    int arr[5];
+
+    // asking the user to insert the value in the array(to store)
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+    // printing the value stored in index 3
+    cout << arr[3];
+
+    // changing the stored value in the index 1
+    arr[1] = 17;
+    cout << arr[1];
+    return 0;
+}
+
+// input - arr[5] = {2,4,6,8,10}
+// output - 8
+// output - 17
