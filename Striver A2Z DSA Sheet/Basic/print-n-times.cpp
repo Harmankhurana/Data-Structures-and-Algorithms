ea2024ce004574cfd7e4a6999b7a61037c5e4b73
Harman khurana <work.harmankhurana@gmail.com>
2026-05-08 13:32:57 +0530

    Printing n numbers of lines by using loops

---
+++ Striver A2Z DSA Sheet/Basic/print-n-times.cpp
@@ -0,0 +1,17 @@
+// Using loops for printing a line N number of times
+// for loops -
+/*
+    for (intialization ; condition ; increment/decrement) {
+        statements;
+    }
+
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+    for(int i = 1 ; i <= 10 ; i++) {
+        cout << "Harman, You're the best" << endl;
+    }
+    return 0;
+}
