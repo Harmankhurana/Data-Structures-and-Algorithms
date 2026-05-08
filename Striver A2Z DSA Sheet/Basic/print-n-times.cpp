a27f42a11ce4742cdd7422c6d7484bcea066b777
Harman khurana <work.harmankhurana@gmail.com>
2026-05-08 13:44:38 +0530

    Understanding about loops in C++ language

--- Striver A2Z DSA Sheet/Basic/print-n-times.cpp
+++ Striver A2Z DSA Sheet/Basic/print-n-times.cpp
@@ -6,12 +6,30 @@
     }
 
 */
+
+// do while loops - first statement always runs, then checks the condition
+/*
+    initialization;
+    do{
+        statement;
+    }while(condition)
+*/
+
 #include<iostream>
 using namespace std;
 
 int main() {
+    // for loops syntax for printing n times
     for(int i = 1 ; i <= 10 ; i++) {
         cout << "Harman, You're the best" << endl;
     }
+
+    // do while loops syntax for printing n times
+    int i = 1;
+    do{
+        cout << "Harman, You're the best" << endl;
+        i++;
+    } while(i <= 10);
+
     return 0;
 }
