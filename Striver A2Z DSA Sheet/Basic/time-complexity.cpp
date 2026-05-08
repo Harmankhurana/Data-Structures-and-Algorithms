9902adbc9721880f8b273520f1df4966098ae964
Harman khurana <work.harmankhurana@gmail.com>
2026-05-08 15:32:48 +0530

    Finding the time complexity of the given code

--- Striver A2Z DSA Sheet/Basic/time-complexity.cpp
+++ Striver A2Z DSA Sheet/Basic/time-complexity.cpp
@@ -1,3 +1,19 @@
 // What is time complexity?
 // Rate at which the time taken increases with respect to input size is called time complexity
 // 3 Rules for analyzing time complexity - 1. TC should be computed with worst case scenario, 2. Avoid constants, 3. Avoid Lower values
+
+// find the time complexity of the given programs
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    // Time complexity --> O(n*n) or O(n2)
+    for (int i = 0 ; i <= n ; i++) {
+        for (int j = 0 ; i <=n ; j++) {
+            cout << "Harman";
+        }
+    }
+}
