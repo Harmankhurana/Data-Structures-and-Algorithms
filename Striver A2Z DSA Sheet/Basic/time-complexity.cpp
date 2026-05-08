c689eda6868327b448101e9d532b7578fa76140e
Harman khurana <work.harmankhurana@gmail.com>
2026-05-08 15:12:07 +0530

    Understanding about time complexity

--- Striver A2Z DSA Sheet/Basic/time-complexity.cpp
+++ Striver A2Z DSA Sheet/Basic/time-complexity.cpp
@@ -1,2 +1,3 @@
 // What is time complexity?
 // Rate at which the time taken increases with respect to input size is called time complexity
+// 3 Rules for analyzing time complexity - 1. TC should be computed with worst case scenario, 2. Avoid constants, 3. Avoid Lower values
