02ff01b2c1661905d5f9436c481d1188c88adcab
Harman khurana <work.harmankhurana@gmail.com>
2026-05-08 14:31:26 +0530

    Creating a Min Max function

--- Striver A2Z DSA Sheet/Basic/min-max.cpp
+++ Striver A2Z DSA Sheet/Basic/min-max.cpp
@@ -5,21 +5,21 @@ using namespace std;
 // function that checks Min Max values of the given integer
 void MinMax(int a, int b) {
     if (a > b) {
-        cout << a << " is a bigger integer" << endl;
+        cout << a << " is a bigger number" << endl;
     }
     else if (a == b){
-        cout << "both integers are having same value" << endl;
+        cout << "both numbers are having same value" << endl;
     }
     else {
-        cout << b << " is a bigger integer" << endl;
+        cout << b << " is a bigger number" << endl;
     }
 }
 
 int main() {
     int a, b;
-    cout << "Enter first integer" << endl;
+    cout << "Enter first integer: " << endl;
     cin >> a;
-    cout << "Enter second integer" << endl;
+    cout << "Enter second integer: " << endl;
     cin >> b;
 
     MinMax(a, b);
