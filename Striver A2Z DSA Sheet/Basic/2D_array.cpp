7356fbf5035abe42fe30dcc74cfb77dd993a7604
Harman khurana <work.harmankhurana@gmail.com>
2026-05-10 23:18:18 +0530

    refactor: rename files to use underscore for better readability

---
+++ Striver A2Z DSA Sheet/Basic/2D_array.cpp
@@ -0,0 +1,13 @@
+// Understanding about 2D array
+#include<iostream>
+using namespace std;
+
+int main() {
+    // creating a 2D array
+    int arr[3][5];
+    // storing a value in index(2,3)
+    arr[2][3] = 78;
+    // printing the stored value
+    cout << arr[2][3];
+    return 0;
+}
