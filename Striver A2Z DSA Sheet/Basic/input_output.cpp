2b30894c6bbd6a1e544b00ae77f9d7b3eafb6491
Harman khurana <work.harmankhurana@gmail.com>
2026-05-10 23:23:49 +0530

    refactor: rename files to use underscore for better readability

---
+++ Striver A2Z DSA Sheet/Basic/input_output.cpp
@@ -0,0 +1,16 @@
+// Writing Input/Output
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+
+    cout << "Enter the Value of X: ";
+    cin >> x;
+
+    cout << "Enter the value of Y: ";
+    cin >> y;
+    
+    cout << "Value of X is: " << x << "Value of Y is: " << y;
+    return 0;
+}
