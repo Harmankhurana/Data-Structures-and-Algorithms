98b08943eff5ea3c62aec2d9b9246c5669e29069
Harman khurana <work.harmankhurana@gmail.com>
2026-05-11 14:27:42 +0530

    feat: add increasing number triangle pattern using function

--- Striver A2Z DSA Sheet/Basic/number_right_triangle_pattern.cpp
+++ Striver A2Z DSA Sheet/Basic/number_right_triangle_pattern.cpp
@@ -1,11 +1,11 @@
-// A program which takes input for number of rows and prints a right triangle star pattern
+// A program which takes input for number of rows and prints a right triangle number pattern
 #include<iostream>
 using namespace std;
 
 void rightTrianglePattern(int n) {
     for (int i = 1 ; i <= n ; i++) {
         for (int j = 1 ; j <= i ; j++) {
-            cout << "* ";
+            cout << j;
         }
         cout << endl;
     }
@@ -25,9 +25,9 @@ int main() {
 // Enter the value of n: 5
 // output -
 /*
-    *
-    * * 
-    * * * 
-    * * * *
-    * * * * * 
+    1
+    12
+    123
+    1234
+    12345
 */
