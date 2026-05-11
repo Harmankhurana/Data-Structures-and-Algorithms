61a92606b0be3ac4e61d275a6b129e1af19b388b
Harman khurana <work.harmankhurana@gmail.com>
2026-05-11 15:38:27 +0530

    Add right triangle pattern with repeated row numbers

--- Striver A2Z DSA Sheet/Basic/same_number_triangle_pattern.cpp
+++ Striver A2Z DSA Sheet/Basic/same_number_triangle_pattern.cpp
@@ -3,7 +3,7 @@
 #include<iostream>
 using namespace std;
 
-void rightTrianglePattern(int n) {
+void sameNumberTrianglePattern(int n) {
     for (int i = 1 ; i <= n ; i++) {
         for (int j = 1 ; j <= i ; j++) {
             cout << i;
@@ -17,7 +17,7 @@ int main() {
     cout << "Enter the value of n: ";
     cin >> n;
 
-    rightTrianglePattern(n);
+    sameNumberTrianglePattern(n);
     return 0;
 
 }
