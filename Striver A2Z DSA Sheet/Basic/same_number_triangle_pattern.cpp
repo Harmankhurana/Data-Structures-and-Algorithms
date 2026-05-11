9ea750418cdf5bd0925843842736e6b978c5e7ee
Harman khurana <work.harmankhurana@gmail.com>
2026-05-11 15:08:17 +0530

    Add right-angled triangle number pattern program

---
+++ Striver A2Z DSA Sheet/Basic/same_number_triangle_pattern.cpp
@@ -0,0 +1,34 @@
+// A program that takes an integer input 'n' (number of rows) and prints a right-angled triangle pattern 
+// where each row contains the same number repeated equal to the row number.
+#include<iostream>
+using namespace std;
+
+void rightTrianglePattern(int n) {
+    for (int i = 1 ; i <= n ; i++) {
+        for (int j = 1 ; j <= i ; j++) {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    rightTrianglePattern(n);
+    return 0;
+
+}
+
+// input - 
+// Enter the value of n: 5
+// output -
+/*
+    1
+    22
+    333
+    4444
+    55555
+*/
