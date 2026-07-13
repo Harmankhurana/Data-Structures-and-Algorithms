b96c5c9dabea82ff493679eabeef1cf0fc789ffb
Harman khurana <work.harmankhurana@gmail.com>
2026-07-14 01:00:05 +0530

    Solving a Recursion problem for printing N to 1 in backtracking

--- Practice/Recursion_N_to_1_Backtracking.cpp
+++ Practice/Recursion_N_to_1_Backtracking.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 using namespace std;
 
-void print(int count, int ) {
-
+void print(int count, int n) {
+    if (count > n) return;
+    print(count + 1, n);
+    cout << count << " ";
 }
 
 int main() {
     int n;
     cout << "Enter You're number here: " << endl;
     cin >> n;
+
+    print(n, n);
 }
