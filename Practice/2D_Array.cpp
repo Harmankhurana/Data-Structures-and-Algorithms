c208bf25460fd213622768205e19a26b62b2510a
Harman khurana <work.harmankhurana@gmail.com>
2026-07-13 11:35:06 +0530

    Practicing Basic problems

---
+++ Practice/2D_Array.cpp
@@ -0,0 +1,10 @@
+// Creating a 2D array and printing out a value from it
+#include<iostream>
+using namespace std;
+
+int main() {
+    // creating a 2D array(matrix)
+    int m[3][4] = {{1,2,3,4} , {5,6,7,8} , {9,10,11,12}};
+    cout << m[1][3] << endl; // 8
+    return 0;
+}
